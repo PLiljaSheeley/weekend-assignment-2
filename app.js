06e5881e147ded903a8760303b7fee49418b64b4
Peter Lilja-Sheeley <pliljasheeley@gmail.com>
2016-04-06 10:16:36 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+var userInput = 0;
+var movie = {};
+
+$(function(){
+
+  $.get('http://www.omdbapi.com/?t=pokemon+the+first+movie&y=&plot=short&r=json').done(function(response){
+    console.log(response);
+    movie = response;
+    $('#img1').attr('src',movie.Poster);
+    $('#movie1').append('<p>' + movie.Title + '</p>')
+    $('#movie1').append('<p>' + movie.Plot + '</p>')
+    });
+  $.get('http://www.omdbapi.com/?t=the+bounty+hunter&y=&plot=short&r=json').done(function(response){
+    movie = response;
+    $('#img2').attr('src',movie.Poster);
+    $('#movie2').append('<p>' + movie.Title + '</p>')
+    $('#movie2').append('<p>' + movie.Plot + '</p>')
+    });
+  $.get('http://www.omdbapi.com/?t=frozen&y=&plot=short&r=json').done(function(response){
+    movie = response;
+    $('#img3').attr('src',movie.Poster);
+    $('#movie3').append('<p>' + movie.Title + '</p>')
+    $('#movie3').append('<p>' + movie.Plot + '</p>')
+    });
+
+  $("#showMovie1").on("click", function(){
+    $("#movie1").show();
+  });
+  $("#showMovie2").on("click", function(){
+    $("#movie2").show();
+  });
+  $("#showMovie3").on("click", function(){
+    $("#movie3").show();
+  });
+    $("#hideMovie1").on("click", function(){
+    $("#movie1").hide();
+  });
+  $("#hideMovie2").on("click", function(){
+    $("#movie2").hide();
+  });
+  $("#hideMovie3").on("click", function(){
+    $("#movie3").hide();
+  });
+    
+
+  $("#getMovie").on("click", function(){
+    getUserInput();
+    getTheMovie(); 
+  });
+
+  function getTheMovie(){
+    $.get('http://www.omdbapi.com/?t=' + userInput + '&y=&plot=short&r=json').done(function(response){
+      console.log(response);
+      movie = response;
+      $('.userMovie').append("<img id='newMovie' src=''>")
+      $('#newMovie').attr('src',movie.Poster);
+      });
+    }
+
+
+  function getUserInput(){
+    //clear();
+    
+    userInput = $("#theInput").val();
+    $("#theInput").val("userInput");
+    return (userInput);
+  }
+  })
+
+  // function clear(){
+  //   ('.userMovie').remove();
+  // }
